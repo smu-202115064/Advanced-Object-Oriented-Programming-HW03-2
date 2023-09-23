6ce0691bf3682bd93dafab17fd17c84b7bc4da19
Hepheir <hepheir@gmail.com>
2023-09-24 03:24:52 +0900

    Create InsertionSortAlg (using code of SortAlgs.java)

---
+++ src/InsertionSortAlg.java
@@ -0,0 +1,12 @@
+public class InsertionSortAlg implements SortAlg {
+    public void sort(int[] data) {
+        for (int i = 1; i < data.length; i++) {
+            int key = data[i];
+            int j;
+            for (j = i - 1; j >= 0 && data[j] > key; j--) {
+                data[j + 1] = data[j];
+            }
+            data[j + 1] = key;
+        }
+    }
+}
