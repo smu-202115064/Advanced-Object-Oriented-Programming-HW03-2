e037d30aa3127b93ee366b09d4c02ff8a8b33867
Hepheir <hepheir@gmail.com>
2023-09-24 03:24:43 +0900

    Create BubbleSortAlg (using code of SortAlgs.java)

---
+++ src/BubbleSortAlg.java
@@ -0,0 +1,13 @@
+public class BubbleSortAlg implements SortAlg {
+    public void sort(int[] data) {
+        for (int i = 0; i < data.length - 1; i++) {
+            for (int j = 0; j < data.length - 1; j++) {
+                if (data[j] > data[j + 1]) { // swap
+                    int temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
